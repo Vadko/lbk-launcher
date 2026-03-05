3c0093f5b5933917bc3117beb255e551288f3f85
Vadko <vpoglid@gmail.com>
2026-03-05 03:10:51 +0200

    add reporter

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   timeout: 120_000,
   retries: 1,
   workers: 1,
-  reporter: [['list'], ['html', { open: 'never' }]],
+  reporter: [['list'], ['html', { open: 'never' }], ['json', { outputFile: 'results.json' }]],
   use: {
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
