d970d09a524d96e2b2b39236198b6369bf8a2a0a
MaksymKurny <maksym.2029@gmail.com>
2026-04-22 22:48:29 +0300

    refactor: remove unused Button import in SendLogsModal

--- src/renderer/components/Modal/SendLogsModal.tsx
+++ src/renderer/components/Modal/SendLogsModal.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { trackEvent } from '../../utils/analytics';
-import { Button } from '../ui/Button';
 import { Modal } from './Modal';
 
 interface SendLogsModalProps {
