c30674cd051ea078ba8e002c29333cd4d79de112
Vadko <vpoglid@gmail.com>
2026-05-14 05:19:06 +0300

    cleanup

--- src/types/binarykvparser.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'binarykvparser' {
-  // Steam's binary KeyValues format is schema-less. Callers narrow downstream.
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  export function parse(buffer: Buffer | Uint8Array, offset?: number): any;
-  /* eslint-enable @typescript-eslint/no-explicit-any */
-}
