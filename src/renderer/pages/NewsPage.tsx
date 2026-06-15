13351931573f7fd526fb1f9f179f9eac1433ab6e
Vadko <vpoglid@gmail.com>
2026-06-16 00:54:36 +0300

    e2e + fixes

--- src/renderer/pages/NewsPage.tsx
+++ src/renderer/pages/NewsPage.tsx
@@ -19,7 +19,7 @@ export const NewsPage: React.FC = () => {
   return (
     <div
       data-gamepad-main-content
-      className={`flex-1 grid grid-cols-1 justify-items-center items-start px-8 ${useGamepadModeStore.getState().isGamepadMode && 'py-4'} overflow-y-auto justify-center custom-scrollbar scrollbar-gutter-[stable]`}
+      className={`flex-1 grid grid-cols-1 justify-items-center items-start px-8 ${useGamepadModeStore.getState().isGamepadMode && 'py-4'} overflow-y-auto justify-center custom-scrollbar`}
     >
       <div
         className={`main-page grid grid-rows-auto grid-cols-1 gap-10 h-auto w-full max-w-[1317px]`}
